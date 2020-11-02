3424aa21a8250cff63ca34d5ef1ee709b017b487
mark bright baraka <markbrightbaraka@gmail.com>
2020-11-02 16:42:21 +0300

    update register form

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Mark Bright Baraka',
+            'email' => 'mark@gmail.com',
+            'password' => Hash::make('helloworld'),
+            'role' => '1'
+        ]);
+    }
+}
