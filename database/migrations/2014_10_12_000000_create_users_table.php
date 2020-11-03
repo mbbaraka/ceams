8eadee0f88268bb9a53d5a2bff2c6ba31959a569
mark bright baraka <markbrightbaraka@gmail.com>
2020-11-03 14:39:29 +0300

    updates

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('avator')->nullable();
             $table->string('password');
             $table->string('phone')->nullable();
             $table->string('dob')->nullable();
