e5f34d7a3bf18bdb3aebd2ce97b987c6ca2e73c5
mark bright baraka <markbrightbaraka@gmail.com>
2020-11-27 13:03:45 +0300

    changes to blade which sends email for assigning

--- resources/views/mail/role-assign.blade.php
+++ resources/views/mail/role-assign.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Role Assigned!.
 
-Dear {{ $first_name }},
+Dear {{ $name }},
 <br>
 You've been assigned a role of {{ $role }} by the Human Resource. Please you can no =w start exercising your duties
 as the {{ $role }}
